82b3b2344a20d6a6f3008e0b6cbb90fc4a2a2474
Anna Goetz <anna.gtz087@outlook.com>
2023-09-24 15:58:58 +0200

    changed index

--- script.js
+++ script.js
@@ -36,14 +36,14 @@ $(document).ready(function(){
   // typing text animation script
   var typed = new Typed(".typing", {
       //strings: ["YouTuber", "Developer", "Blogger", "Designer", "Freelancer"],//
-      strings: ["Renewables Analyst"],
+      strings: ["Project Controller"],
       typeSpeed: 100,
       backSpeed: 60,
       loop: true
   });
 
   var typed = new Typed(".typing-2", {
-      strings: ["Renewables Analyst"],
+      strings: ["Project Controller"],
       typeSpeed: 100,
       backSpeed: 60,
       loop: true
